ac4ac3220c5590492e0e2b6de0ba2f4f978cff05
stephaniee3e <158521537+stephaniee3e@users.noreply.github.com>
2025-12-10 14:01:49 +0000

    FizzBuzz

---
+++ main.py
@@ -0,0 +1,13 @@
+for second in range (100):
+   if second % 3 == 0 and second % 5 == 0:
+    print("FizzBuzz")
+   elif second % 3 == 0:
+    print("Fizz")
+   elif second % 5 == 0:
+    print("Buzz")
+   else:
+    print (second)
+print(second +1)    
+    
+
+    
